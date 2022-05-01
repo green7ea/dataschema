41d1ffa6fa2c471a1bc77971ffcebcd4a8cf2530
Emmanuel Boudreault <emmanuel.boudreault@polymtl.ca>
2022-05-01 11:50:51 +0200

    Use tuples instead of objects with a 'type' to identify oneOf
    When 'oneOf' was used, it injected 'type' into the objects that it
    could represent.
    
    One problem was that 'type' could class with an existing object
    property.
    
    Another problem is that it assumed that everything in a 'oneOf' was an
    object.
    
    A third problem is that this doesn't work well if we want a binary
    serialization.
    
    By using a tuple instead, all these problems are fixed. At first, I
    feared that writing schemas by hand wouldn't be as pleasant but it
    turned out that it's actually nicer :-).
    
    The generated typescript needed a newer version of typescript for the
    narrowing to work properly.

--- core/src/generate.ts
+++ core/src/generate.ts
@@ -1,30 +1,43 @@
 import _ from "lodash";
 
-import { ds_Definition, ds_Schema } from "./generated_types";
+import {
+  ds_Array,
+  ds_Definition,
+  ds_Map,
+  ds_Object,
+  ds_OneOf,
+  ds_Ref,
+  ds_Schema,
+  ds_Tuple,
+} from "./generated_types";
 
 export function ds_file_to_typescript(def: ds_Definition): string {
   const imports = imports_to_typescript(def.imports);
   const definitions = definition_to_typescript(def.definitions);
 
-  return imports + '\n\n' + definitions;
+  return imports + "\n\n" + definitions;
 }
 
 function imports_to_typescript(imports?: Map<string, string[]>): string {
   return _(imports)
     .toPairs()
     .map(([file, symbols]) => {
-      const symbols_string = _.join(symbols, ', ');
+      const symbols_string = _.join(symbols, ", ");
 
       return `import ${symbols_string} from '${file}';`;
     })
-    .join('\n\n');
+    .join("\n\n");
 }
 
-function definition_to_typescript(definitions?: Map<string, ds_Schema>): string {
+function definition_to_typescript(
+  definitions?: Map<string, ds_Schema>
+): string {
   return _(definitions)
     .toPairs()
-    .map(([name, schema]) => {
-      switch (schema.type) {
+    .map(([name, schema]: [string, ds_Schema]) => {
+      const [schema_type, schema_value] = schema;
+
+      switch (schema_type) {
         case "string":
         case "number":
         case "integer":
@@ -32,20 +45,23 @@ function definition_to_typescript(definitions?: Map<string, ds_Schema>): string 
         case "map":
         case "array":
         case "oneOf":
-        case "oneOfType":
         case "enum":
         case "ref":
           return `export type ${name} = ${schema_to_typescript(schema)};`;
         case "object":
+          const obj = schema_value;
           const declaration = `export interface ${name}`;
-          if (schema.extends) {
-            const supers = _(schema.extends).join(', ');
-            const obj = schema_to_typescript({
-              ...schema,
-              extends: undefined,
-            });
+          if (obj.extends) {
+            const supers = _(obj.extends).join(", ");
+            const obj_ts = schema_to_typescript([
+              "object",
+              {
+                ...obj,
+                extends: undefined,
+              },
+            ]);
 
-            return `${declaration} extends ${supers} ${obj}`
+            return `${declaration} extends ${supers} ${obj_ts}`;
           }
 
           return `${declaration} ${schema_to_typescript(schema)}`;
@@ -57,7 +73,9 @@ function definition_to_typescript(definitions?: Map<string, ds_Schema>): string 
 }
 
 function schema_to_typescript(schema: ds_Schema): string {
-  switch (schema.type) {
+  const [schema_type, schema_value] = schema;
+
+  switch (schema_type) {
     case "string":
       return "string";
     case "number":
@@ -67,16 +85,21 @@ function schema_to_typescript(schema: ds_Schema): string {
     case "boolean":
       return "boolean";
     case "map":
-      const key = schema_to_typescript(schema.key);
-      const value = schema_to_typescript(schema.value);
+      const key = schema_to_typescript(schema_value.key);
+      const value = schema_to_typescript(schema_value.value);
       return `Map<${key}, ${value}>`;
     case "array":
-      return `${schema_to_typescript(schema.items)}[]`;
+      return `${schema_to_typescript(schema_value.items)}[]`;
+    case "tuple":
+      const items = _(schema_value.items)
+        .map((item) => schema_to_typescript(item))
+        .join(", ");
+      return `[${items}]`;
     case "object":
-      const props = _(schema.properties)
+      const props = _(schema_value.properties)
         .toPairs()
         .map(([name, value]) => {
-          const optional = _(schema.required).some((req) => req === name)
+          const optional = _(schema_value.required).some((req) => req === name)
             ? ""
             : "?";
 
@@ -85,38 +108,23 @@ function schema_to_typescript(schema: ds_Schema): string {
         .join("\n");
 
       const obj_string = `{\n${props}\n}`;
-      return schema.extends
-        ? _([...schema.extends, obj_string]).join(' & ')
+      return schema_value.extends
+        ? _([...schema_value.extends, obj_string]).join(" & ")
         : obj_string;
     case "enum":
       return `enum []`;
     case "oneOf": {
-      const tag = schema.tag || "type";
-
-      return _(schema.these)
+      return _(schema_value.these)
         .toPairs()
         .map(
-          ([name, value]) =>
-            `| { ${tag}: '${name}' } & ${schema_to_typescript(value)}`
+          ([name, value]: [string, ds_Schema]) =>
+            `| ['${name}', ${schema_to_typescript(value)}]`
         )
         .join("\n");
     }
-    case "oneOfType":
-      const types = _(schema.these)
-        .sortBy((x) => x.type)
-        .sortedUniq()
-        .value();
-
-      if (schema.these.length !== types.length) {
-        throw "uh oh, oneOfType doesn't have unique types";
-      }
-
-      return _(types)
-        .map((type) => `| ${schema_to_typescript(type)}`)
-        .join("\n");
     case "ref":
-      return schema.to;
+      return schema_value.to;
     default:
-      return `// Unknown type`;
+      return `// Unknown type '${schema_type}'`;
   }
 }
