8586a0e85533fdc1a492c6c5273aa12af0a177ea
Emmanuel Boudreault <emmanuel.boudreault@polymtl.ca>
2022-05-27 16:10:51 +0200

    Put generated code in files instead of printing to stdout

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,19 +1,43 @@
 import _ from "lodash";
 
 import { ds_file_to_typescript } from "./generate";
-import { read_def_from_file } from "./reader";
 import { format } from "prettier";
+import { read_def_from_file } from "./reader";
+import { promises as fs } from "fs";
+import path from "path";
 
-const filename = _.last(process.argv) || "";
+const languages = {
+  ts: {
+    dir: "ts-gen",
+    extension: "ts",
+    fn: ds_file_to_typescript,
+    pretty: (code: string) => format(code, { parser: "typescript" }),
+  },
+};
 
 async function main() {
-  const data = await read_def_from_file(filename);
-  const typescript = ds_file_to_typescript(data);
+  const filenames = _(process.argv).drop(2).value() || [];
+  const langs = ["ts"];
 
-  console.log(typescript);
+  filenames.forEach(async (file) => {
+    const data = await read_def_from_file(file);
+    const path_obj = path.parse(file);
 
-  // const pretty = format(typescript, { parser: "typescript" });
-  // console.log(pretty);
+    langs.forEach((lang) => {
+      // TODO as any eww
+      const lang_obj = (languages as any)[lang];
+
+      const code = lang_obj.fn(data);
+      const pretty = lang_obj.pretty(code);
+
+      // TODO we should output to pwd and not relative to each file
+      const output_folder = path.join(path_obj.dir, lang_obj.dir);
+      const output_file = `${path_obj.name}.${lang_obj.extension}`;
+      const output_path = path.join(output_folder, output_file);
+
+      fs.writeFile(output_path, pretty);
+    });
+  });
 }
 
 main();
